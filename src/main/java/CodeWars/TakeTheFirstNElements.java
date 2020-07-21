30ffb68610295ac1cd5eedb0ed3ffbd0e6b44cda
duolist <neohomosapiens@gmail.com>
2020-07-21 16:25:39 +0300

    Enumerable Magic #25 - Take the First N Elements https://www.codewars.com/kata/545afd0761aa4c3055001386/solutions/java

---
+++ src/main/java/CodeWars/TakeTheFirstNElements.java
@@ -0,0 +1,16 @@
+package CodeWars;
+
+import java.util.Arrays;
+
+public class TakeTheFirstNElements {
+    public static int[] take(int[] arr, int n) {
+        int[] result = new int[0];
+        if (n > arr.length) {
+            n = arr.length;
+        }
+        if (arr.length > 0) {
+            result = Arrays.copyOfRange(arr, 0, n);
+        }
+        return result;
+    }
+}
