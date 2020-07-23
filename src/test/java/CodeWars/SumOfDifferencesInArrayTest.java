a65ad58c1258c5e7ac157cfa516133f9124f3292
duolist <neohomosapiens@gmail.com>
2020-07-23 18:10:39 +0300

    Sum of differences in array https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/solutions/java

---
+++ src/test/java/CodeWars/SumOfDifferencesInArrayTest.java
@@ -0,0 +1,20 @@
+package CodeWars;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SumOfDifferencesInArrayTest {
+
+    @Test
+    void sumOfDifferences() {
+        assertEquals(9, SumOfDifferencesInArray.sumOfDifferences(new int[]{1, 2, 10}));
+        assertEquals(2, SumOfDifferencesInArray.sumOfDifferences(new int[]{-3, -2, -1}));
+        assertEquals(0, SumOfDifferencesInArray.sumOfDifferences(new int[]{1, 1, 1, 1, 1}));
+        assertEquals(34, SumOfDifferencesInArray.sumOfDifferences(new int[]{-17, 17}));
+        assertEquals(0, SumOfDifferencesInArray.sumOfDifferences(new int[0]));
+        assertEquals(0, SumOfDifferencesInArray.sumOfDifferences(new int[]{0}));
+        assertEquals(0, SumOfDifferencesInArray.sumOfDifferences(new int[]{-1}));
+        assertEquals(0, SumOfDifferencesInArray.sumOfDifferences(new int[]{1}));
+    }
+}
