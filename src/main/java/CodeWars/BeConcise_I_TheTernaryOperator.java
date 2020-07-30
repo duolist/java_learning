b3d5122b9914342dfa87a194dfee5031c7eb5837
duolist <neohomosapiens@gmail.com>
2020-07-30 18:24:18 +0300

    Be Concise I - The Ternary Operator https://www.codewars.com/kata/56f3f6a82010832b02000f38/solutions/java

---
+++ src/main/java/CodeWars/BeConcise_I_TheTernaryOperator.java
@@ -0,0 +1,16 @@
+package CodeWars;
+
+public class BeConcise_I_TheTernaryOperator {
+    public static String describeAge(int age) {
+//        if (age <= 12) {
+//            return "You're a(n) kid";
+//        } else if (age >= 13 && age <= 17) {
+//            return "You're a(n) teenager";
+//        } else if (age >= 18 && age <= 64) {
+//            return "You're a(n) adult";
+//        } else {
+//            return "You're a(n) elderly";
+//        }
+        return "You're a(n) " + (age > 64 ? "elderly" : age > 17 ? "adult" : age > 12 ? "teenager" : "kid");
+    }
+}
