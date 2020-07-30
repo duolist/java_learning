4cffb5cf4ca2b6a4a425085abcf8f00dfad43799
duolist <neohomosapiens@gmail.com>
2020-07-30 17:49:13 +0300

    Some commits about Nested interfaces

--- src/main/java/lambda_functionalInterfaces_0/InternalInterfacesExample.java
+++ src/main/java/lambda_functionalInterfaces_0/InternalInterfacesExample.java
@@ -2,6 +2,13 @@ package lambda_functionalInterfaces_0;
 
 public class InternalInterfacesExample {
     public static void main(String[] args) {
+
+        /*
+          Класс A содержит интерфейс. Класс B имплементирует интерфейс класса A и имеет конкретную его реализацию
+          Потом мы в конкретный эксземпляр итрефейса класса А помещаем новый экземпляр класса B
+          с конкретной реализацией интерфейса
+         */
+
         A.NestedIf nif = new B();
         if (nif.isNotNegative(10)) {
             System.out.println("Число 10 не отрицательное.");
